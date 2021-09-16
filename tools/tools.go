1b7078a724d493b6ae37ce6e340368e6962edcf1
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-09-16 12:23:58 +0100

    Update import paths for mock-hubble

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@ package tools
 import (
 	_ "github.com/cloudflare/cfssl/cmd/cfssl"
 	_ "github.com/cloudflare/cfssl/cmd/cfssljson"
-	_ "github.com/isovalent/hubble-toolbox/pkg/hubble/mock/server"
+	_ "github.com/isovalent/mock-hubble/server"
 	_ "github.com/open-telemetry/opentelemetry-collector-builder"
 	_ "go.opentelemetry.io/collector/cmd/otelcol"
 )
