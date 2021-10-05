ab946b51a9642dec2b2a8798ccade23045333673
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-10-05 21:12:13 +0100

    Fix and improve flow encoding
    - add test to validate encoding by roundtripping the data
    - cleanup code
    - get rid of jsonpb, use new protojson instead
    - fix usage of `bytes.Buffer`
    - fix handling of `protoreflect.DoubleKind`
    - implement handling of special cases for `google.protobuf.Timestamp`
      and `google.protobuf.BoolValue` types
    - upgrade dependencies

--- common/common_test.go
+++ common/common_test.go
@@ -2,12 +2,17 @@ package common_test
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+
+	commonV1 "go.opentelemetry.io/proto/otlp/common/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/reflect/protoreflect"
 
+	"github.com/cilium/cilium/api/v1/flow"
 	"github.com/isovalent/hubble-otel/common"
 	"github.com/isovalent/hubble-otel/logconv"
 	"github.com/isovalent/hubble-otel/receiver"
@@ -87,4 +92,131 @@ func BenchmarkAllModes(b *testing.B) {
 			})
 		}
 	}
+}
+
+func TestRoudtripEncoding(t *testing.T) {
+	log := logrus.New()
+	//log.SetLevel(logrus.DebugLevel)
+
+	encodingFormats := []string{
+		common.EncodingJSON,
+		common.EncodingTypedMap,
+	}
+
+	encodingOptions := []common.EncodingOptions{
+		{TopLevelKeys: false, LabelsAsMaps: false, LogPayloadAsBody: false},
+	}
+
+	samples := []string{
+		"basic-sample-10-flows.json",
+		"basic-sample-330-dns-flows.json",
+		"basic-sample-348-http-flows.json",
+	}
+
+	for s := range samples {
+		for e := range encodingFormats {
+			for o := range encodingOptions {
+				sample := samples[s]
+				options := encodingOptions[o]
+				options.Encoding = encodingFormats[e]
+
+				c := &common.FlowEncoder{
+					EncodingOptions: options,
+					Logger:          log,
+				}
+
+				t.Run("("+sample+")/"+options.Encoding+":"+options.String(), func(t *testing.T) {
+					for _, f := range testutil.GetFlowSamples(t, "../testdata/"+sample) {
+						v, err := c.ToValue(f)
+						if err != nil {
+							t.Error(err)
+						}
+						if v == nil {
+							t.Error("value cannot be nil")
+						}
+
+						result := []byte{}
+
+						source, err := common.MarshalJSON(f.GetFlow())
+						if err != nil {
+							t.Error(err)
+						}
+						switch options.Encoding {
+						case common.EncodingTypedMap:
+							// turn commonV1.AnyValue into interface{},
+							// and ecode as JSON using standard codec
+							data, err := json.Marshal(toRaw(v.GetValue()))
+							if err != nil {
+								t.Error(err)
+							}
+							// decode JSON into a flow.Flow
+							f := &flow.Flow{}
+							if err = json.Unmarshal(data, f); err != nil {
+								t.Error(err)
+							}
+							// re-encode using funky protobuf encoder that
+							// turns int64 & unint64 into strings and has
+							// other peculiar features...
+							result, err = common.MarshalJSON(f)
+							if err != nil {
+								t.Error(err)
+							}
+						case common.EncodingJSON:
+							result = []byte(v.GetStringValue())
+						}
+
+						sourceData := string(source)
+						if len(sourceData) == 0 {
+							t.Errorf("encoded source cannot be empty")
+						}
+						resultData := string(result)
+						if len(resultData) == 0 {
+							t.Errorf("encoded result cannot be empty")
+						}
+
+						require.JSONEq(t, sourceData, resultData)
+						// t.Logf("source = %s", sourceData)
+						// t.Logf("result = %s", resultData)
+					}
+				})
+			}
+		}
+	}
+}
+
+func toRaw(v interface{}) interface{} {
+	switch v.(type) {
+	case *commonV1.AnyValue_StringValue:
+		return v.(*commonV1.AnyValue_StringValue).StringValue
+	case *commonV1.AnyValue_IntValue:
+		return v.(*commonV1.AnyValue_IntValue).IntValue
+	case *commonV1.AnyValue_DoubleValue:
+		return v.(*commonV1.AnyValue_DoubleValue).DoubleValue
+	case *commonV1.AnyValue_BoolValue:
+		return v.(*commonV1.AnyValue_BoolValue).BoolValue
+	case *commonV1.AnyValue_BytesValue:
+		return v.(*commonV1.AnyValue_BytesValue).BytesValue
+	case *commonV1.AnyValue_KvlistValue:
+		return keyValueListToRaw(v.(*commonV1.AnyValue_KvlistValue).KvlistValue)
+	case *commonV1.AnyValue_ArrayValue:
+		return arrayValueToRaw(v.(*commonV1.AnyValue_ArrayValue).ArrayValue)
+	default:
+		return nil
+	}
+}
+
+func keyValueListToRaw(m *commonV1.KeyValueList) map[string]interface{} {
+	raw := make(map[string]interface{})
+	for _, entry := range m.Values {
+		raw[entry.Key] = toRaw(entry.Value.GetValue())
+	}
+	return raw
+}
+
+func arrayValueToRaw(l *commonV1.ArrayValue) []interface{} {
+	raw := make([]interface{}, len(l.Values))
+	for index, entry := range l.Values {
+		raw[index] = toRaw(entry.GetValue())
+	}
+	return raw
 }
--- go.mod
+++ go.mod
@@ -6,17 +6,17 @@ require (
 	github.com/cilium/cilium v1.10.4
 	github.com/cilium/hubble v0.8.2
 	github.com/dgraph-io/badger/v3 v3.2103.0
-	github.com/gogo/protobuf v1.3.2
 	github.com/isovalent/mock-hubble v0.0.0-20210928133358-8a1660dd0897
 	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusexporter v0.36.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver v0.36.0
 	github.com/prometheus/client_model v0.2.1-0.20200623203004-60555c9708c7
 	github.com/prometheus/common v0.30.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/collector v0.36.0
-	go.opentelemetry.io/otel/sdk v1.0.0-RC3 // indirect
-	go.opentelemetry.io/otel/trace v1.0.0-RC3
-	go.opentelemetry.io/proto/otlp v0.7.0
+	go.opentelemetry.io/otel/sdk v1.0.1 // indirect
+	go.opentelemetry.io/otel/trace v1.0.1
+	go.opentelemetry.io/proto/otlp v0.9.0
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
