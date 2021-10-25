15eb3f0a0886746a277b2ccfe2db2778eec3985d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-10-25 12:32:38 +0100

    Implement receiver plugin

---
+++ receiver/plugin/config.go
@@ -0,0 +1,39 @@
+package hubblereceiver
+
+import (
+	"errors"
+
+	"github.com/isovalent/hubble-otel/common"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/configtls"
+)
+
+type Config struct {
+	config.ReceiverSettings    `mapstructure:",squash"`
+	configtls.TLSClientSetting `mapstructure:"tls,omitempty"`
+
+	Endpoint   string `mapstructure:"endpoint"`
+	BufferSize int    `mapstructure:"buffer_size"`
+
+	FlowEncodingOptions FlowEncodingOptions `mapstructure:"flow_encoding_options"`
+}
+
+type FlowEncodingOptions struct {
+	Traces common.EncodingOptions `mapstructure:"traces"`
+	Logs   common.EncodingOptions `mapstructure:"logs"`
+}
+
+var _ config.Receiver = (*Config)(nil)
+
+func (cfg Config) Validate() error {
+	if cfg.Endpoint == "" {
+		return errors.New("hubble endpoint must be specified")
+	}
+	if err := cfg.FlowEncodingOptions.Traces.ValidForTraces(); err != nil {
+		return err
+	}
+	if err := cfg.FlowEncodingOptions.Logs.ValidForLogs(); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ receiver/plugin/config_test.go
@@ -0,0 +1,34 @@
+package hubblereceiver
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/config/configtest"
+)
+
+func TestLoadConfig(t *testing.T) {
+	factories, err := componenttest.NopFactories()
+	assert.NoError(t, err)
+
+	factory := NewFactory()
+	factories.Receivers[typeStr] = factory
+	cfg, err := configtest.LoadConfigAndValidate(path.Join(".", "testdata", "config.yaml"), factories)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	assert.Equal(t, len(cfg.Receivers), 2)
+
+	r0 := cfg.Receivers[config.NewComponentID(typeStr)]
+	r0.(*Config).Endpoint = ""
+	assert.Equal(t, r0, factory.CreateDefaultConfig())
+
+	r1 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "customname")].(*Config)
+	assert.Equal(t, r1.ReceiverSettings, config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "customname")))
+
+}
