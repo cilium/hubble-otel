d2bb41769f49349cde6a05dd664162865b07c1c5
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-11-09 19:59:05 +0000

    Add explicit assertions regarding inexplicit defaults

--- receiver/config_test.go
+++ receiver/config_test.go
@@ -21,6 +21,9 @@ func TestLoadConfig(t *testing.T) {
 	assert.NoError(t, err)
 
 	factory := NewFactory()
+	defaultConfig := factory.CreateDefaultConfig()
+	defaultEncodingOptions := defaultConfig.(*Config).FlowEncodingOptions
+
 	factories.Receivers[typeStr] = factory
 	cfg, err := configtest.LoadConfigAndValidate(path.Join(".", "testdata", "config.yaml"), factories)
 	require.NoError(t, err)
@@ -30,7 +33,8 @@ func TestLoadConfig(t *testing.T) {
 
 	r0 := cfg.Receivers[config.NewComponentID(typeStr)]
 	r0.(*Config).Endpoint = ""
-	assert.Equal(t, r0, factory.CreateDefaultConfig())
+	assert.Equal(t, r0, defaultConfig)
+	assert.Equal(t, r0.(*Config), defaultConfig.(*Config))
 
 	r1 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "customname")].(*Config)
 	assert.Equal(t, r1.ReceiverSettings, config.NewReceiverSettings(config.NewComponentIDWithName(typeStr, "customname")))
@@ -41,6 +45,14 @@ func TestLoadConfig(t *testing.T) {
 	r3 := cfg.Receivers[config.NewComponentIDWithName(typeStr, "nondefaultopts")].(*Config)
 	assert.Equal(t, r3.Endpoint, "localhost:4244")
 	assert.Equal(t, *r3.FlowEncodingOptions.Traces.Encoding, "JSON")
-	assert.Equal(t, *r3.FlowEncodingOptions.Traces.TopLevelKeys, false)
-	assert.Equal(t, *r3.FlowEncodingOptions.Logs.LogPayloadAsBody, true)
+	assert.Equal(t, *r3.FlowEncodingOptions.Traces.TopLevelKeys, !*defaultEncodingOptions.Traces.TopLevelKeys)
+	assert.Equal(t, *r3.FlowEncodingOptions.Logs.LogPayloadAsBody, !*defaultEncodingOptions.Logs.LogPayloadAsBody)
+
+	// this is to assert that currenly defaulting of unset fields is not based on CreateDefaultConfig(), which
+	// is not ideal, but that is how collector configuration appears to work
+	assert.Equal(t, *r3.FlowEncodingOptions.Traces.HeadersAsMaps, !*defaultEncodingOptions.Traces.HeadersAsMaps)
+	assert.Equal(t, *r3.FlowEncodingOptions.Traces.LabelsAsMaps, !*defaultEncodingOptions.Traces.LabelsAsMaps)
+	assert.Equal(t, *r3.FlowEncodingOptions.Logs.TopLevelKeys, !*defaultEncodingOptions.Logs.TopLevelKeys)
+	assert.Equal(t, *r3.FlowEncodingOptions.Logs.HeadersAsMaps, !*defaultEncodingOptions.Logs.HeadersAsMaps)
+	assert.Equal(t, *r3.FlowEncodingOptions.Logs.LabelsAsMaps, !*defaultEncodingOptions.Logs.LabelsAsMaps)
 }
