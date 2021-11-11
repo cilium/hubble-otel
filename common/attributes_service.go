fea5f01951e6a1946da23026a72b3d3c57ce3c8d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-11-11 16:50:25 +0000

    Account from all supported propositions

--- common/attributes_service.go
+++ common/attributes_service.go
@@ -5,6 +5,7 @@ import (
 
 	flowV1 "github.com/cilium/cilium/api/v1/flow"
 	hubbleLabels "github.com/cilium/hubble-ui/backend/domain/labels"
+	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 )
 
 const (
@@ -20,10 +21,22 @@ func GetServiceAttributes(flow *flowV1.Flow, fallbackServiceName string) []*comm
 	}
 
 	if src := flow.Source; src != nil {
-		srcProps := hubbleLabels.Props(src.Labels)
-		if srcProps.AppName != nil {
+		switch srcProps := hubbleLabels.Props(src.Labels) {
+		case srcProps.AppName != nil:
 			resourceAttributes[OTelAttrServiceName] = *srcProps.AppName
 			resourceAttributes[OTelAttrServiceNamespace] = src.Namespace
+		case srcProps.IsHost:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-host"
+		case srcProps.IsInit:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-init"
+		case srcProps.IsKubeDNS:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-kube-dns"
+		case srcProps.IsPrometheus:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-prometheus"
+		case srcProps.IsRemoteNode:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-remote-node"
+		case srcProps.IsWorld:
+			resourceAttributes[OTelAttrServiceName] = fallbackServiceName + "-world"
 		}
 	}
 
